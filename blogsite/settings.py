17f02261b4f97ff1fb719d2ceec13b40142c711c
sathya-coder <vanicse25@gmail.com>
2023-08-01 15:46:44 +0530

    commit changes

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blogApp'
 ]
 
 ROOT_URLCONF = 'blogsite.urls'
